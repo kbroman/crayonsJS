9ab489a04e8790f22c3c4d856a58fb95328cdf2b
Karl Broman <kbroman@gmail.com>
2015-03-26 06:50:58 +1000

    Remove spaces and newlines from crayons.js

--- R/make_js.R
+++ R/make_js.R
@@ -4,9 +4,15 @@ outfile = "../crayons.js"
 
 library(broman)
 library(RJSONIO)
+
+colors <- RJSONIO::toJSON(brocolors("crayons"))
+colors <- gsub("\\n", "", colors)
+colors <- gsub(": ", ":", colors)
+colors <- gsub(" \\}", "}", colors)
+colors <- gsub("\\{ ", "{", colors)
+
 cat("// colors from http://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors\n\n",
     file=outfile)
-cat("crayons = ", file=outfile, append=TRUE)
-cat(RJSONIO::toJSON(brocolors("crayons")),
-    file=outfile, append=TRUE)
+cat("crayons=", file=outfile, append=TRUE)
+cat(colors, file=outfile, append=TRUE)
 cat(";", file=outfile, append=TRUE)
