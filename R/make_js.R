6fe797cc4ef83c03d8423f19534ef363cb2f4f3e
Karl Broman <kbroman@gmail.com>
2015-03-26 06:59:02 +1000

    Add URL for github repo to crayons.js

--- R/make_js.R
+++ R/make_js.R
@@ -11,8 +11,10 @@ colors <- gsub(": ", ":", colors)
 colors <- gsub(" \\}", "}", colors)
 colors <- gsub("\\{ ", "{", colors)
 
-cat("// colors from http://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors\n\n",
+cat("// crayons.js, https://github.com/kbroman/crayonsJS\n",
     file=outfile)
+cat("// colors from http://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors\n\n",
+    file=outfile, append=TRUE)
 cat("crayons=", file=outfile, append=TRUE)
 cat(colors, file=outfile, append=TRUE)
 cat(";", file=outfile, append=TRUE)
