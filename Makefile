a5bb0ceaca08199e5c34e54cb3f6d91dfc51e48c
Karl Broman <kbroman@gmail.com>
2026-06-07 19:56:44 -0500

    Add crayons.css

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-all: crayons.js crayons.png
+all: crayons.js crayons.png crayons.css
 .PHONY: all
 
 # R options (--vanilla, but without --no-environ)
 R_OPTS=--no-save --no-restore --no-init-file --no-site-file
 
 crayons.js: R/make_js.R
+	cd $(<D);R CMD BATCH $(R_OPTS) $(<F)
+
+crayons.css: R/make_css.R
 	cd $(<D);R CMD BATCH $(R_OPTS) $(<F)
 
 crayons.png: R/make_png.R
---
+++ R/make_css.R
@@ -0,0 +1,18 @@
+# make crayons.css
+
+outfile = "../crayons.css"
+
+library(broman)
+library(jsonlite)
+
+crayons <- broman::brocolors("crayons")
+colors <- paste0("  --", gsub(" ", "", names(crayons)), ": ", crayons, ";")
+colors <- gsub("'", "", colors)
+
+cat("// crayons.css, https://github.com/kbroman/crayonsJS\n",
+    file=outfile)
+cat("// colors from http://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors\n\n",
+    file=outfile, append=TRUE)
+cat(":root {\n", file=outfile, append=TRUE)
+cat(colors, sep="\n", file=outfile, append=TRUE)
+cat("}", file=outfile, append=TRUE)
